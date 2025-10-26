c329ef2514a654ff1718fe7b05ece3a8ad2e81e5
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-10-26 22:25:16 +0530

    Create User Model and add Signup route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 const { connectDB } = require("./config/database.js");
+const UserModel = require("./models/user-models.js");
 
 const app = express();
 
 const PORT = 4000;
 
-app.use("/", (req, res) => {
-  res.send("Hello World");
+app.post("/signup", async (req, res) => {
+  const user = new UserModel({
+    firstName: "Alex",
+    lastName: "Carey",
+    email: "Alex@Carey.com",
+    password: "Alex@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User Created SuccessFully....");
+  } catch (error) {
+    res.status(400).send("Error in creating User", error.message);
+  }
 });
 
 connectDB()
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect(`${process.env.MONGODB_URI}/Fynder`)
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}/Fynder`);
+};
 
 module.exports = {
-    connectDB,
-}
+  connectDB,
+};
