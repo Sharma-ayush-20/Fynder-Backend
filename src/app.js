79a9c8dc2dfe140a5382f3a2bfb10e19887d208a
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-10-27 22:19:17 +0530

    build signup route completely

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 const { connectDB } = require("./config/database.js");
 const UserModel = require("./models/user-models.js");
 
@@ -8,15 +9,43 @@ const PORT = 4000;
 
 app.use(express.json());
 
+//signup route
 app.post("/signup", async (req, res) => {
-  const userData = req.body;
-  const user = new UserModel(userData);
-
   try {
+    const userData = req.body;
+    const { firstName, lastName, email, password } = userData;
+    const ALLOWED_SIGNUP = ["firstName", "lastName", "email", "password"];
+    const isSignupAllow = Object.keys(userData).every((data) =>
+      ALLOWED_SIGNUP.includes(data)
+    );
+    //check only this field should be filled
+    if (!isSignupAllow) {
+      return res.status(400).json({ message: "Signup not allowed.." });
+    }
+    //check field is empty
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: "please Fill all the details" });
+    }
+
+    //encrypt the password
+    const saltRound = 10;
+    const hashPassword = await bcrypt.hash(password, saltRound);
+
+    //create a user
+    const user = new UserModel({
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+    });
+
+    //save in the database
     await user.save();
-    res.send("User Created SuccessFully....");
+    return res.status(200).json({ message: "User Created SuccessFully...." });
   } catch (error) {
-    res.status(400).send(`Error in creating User: ${error.message}`);
+    res
+      .status(500)
+      .json({ message: `Error in creating User: ${error.message}` });
   }
 });
 
@@ -35,25 +64,36 @@ app.get("/feed", async (req, res) => {
 
 app.patch("/updateuser/:userId", async (req, res) => {
   try {
-    const userId = req.params?.userId; 
+    const userId = req.params?.userId;
     const updatedUserData = req.body;
 
-    const ALLOWED_UPDATES = ["userId", "photoUrl", "age", "about", "skills", "gender"] // this thing should be update
+    const ALLOWED_UPDATES = [
+      "userId",
+      "photoUrl",
+      "age",
+      "about",
+      "skills",
+      "gender",
+    ]; // this thing should be update
 
-    const isUpdateAllowed = Object.keys(updatedUserData).every((K) => ALLOWED_UPDATES.includes(K))
+    const isUpdateAllowed = Object.keys(updatedUserData).every((K) =>
+      ALLOWED_UPDATES.includes(K)
+    );
     //in my model fields are there -> K = 1 fields includes in Allowed_updated then it return true
 
-    if(!isUpdateAllowed){
-      throw new Error("Update not allowed..")
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed..");
     }
     // console.log(updatedUserData)
-    const user = await UserModel.findByIdAndUpdate(userId, updatedUserData, {runValidators: true})
-    if(user){
-      res.status(200).send("User Updated SuccessFully....")
-    }else{
-      res.status(400).send("Problem in updating Data")
+    const user = await UserModel.findByIdAndUpdate(userId, updatedUserData, {
+      runValidators: true,
+    });
+    if (user) {
+      res.status(200).send("User Updated SuccessFully....");
+    } else {
+      res.status(400).send("Problem in updating Data");
     }
-  }  catch (error) {
+  } catch (error) {
     res.status(400).send(`Error in updating user: ${error.message}`);
   }
 });
@@ -61,17 +101,16 @@ app.patch("/updateuser/:userId", async (req, res) => {
 app.delete("/deleteuser", async (req, res) => {
   try {
     const userId = req.body.userId;
-    const deleteUser = await UserModel.findByIdAndDelete(userId)
-    if(deleteUser){
-      res.status(200).send("User Deleted SuccessFully...")
-    }
-    else{
-      res.status(400).send("Problem in Deleting user")
+    const deleteUser = await UserModel.findByIdAndDelete(userId);
+    if (deleteUser) {
+      res.status(200).send("User Deleted SuccessFully...");
+    } else {
+      res.status(400).send("Problem in Deleting user");
     }
   } catch (error) {
     res.status(400).send("Error in gettind Users", error.message);
   }
-})
+});
 
 connectDB()
   .then(() => {
