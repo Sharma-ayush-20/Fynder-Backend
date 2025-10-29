45cb3e7e4d08a5fb81be0dd953fd0b4be6b2ada0
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-10-29 12:56:42 +0530

    use query projector using select to not send user password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,15 +2,17 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth.js");
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req,res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
+    //take current user data
     const user = req.user;
-    res.send(user)
+    //send a response
+    res.status(200).json({ user: user });
   } catch (error) {
     res
       .status(500)
-      .json({ message: `Error in profile route: ${error.message}` });
+      .json({ message: `Error in get user profile: ${error.message}` });
   }
-})
+});
 
-module.exports = {profileRouter}
+module.exports = { profileRouter };
