b87b1aa5373f82ea2173c89feae5a39266d5c63e
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-11-01 13:53:43 +0530

    create Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middlewares/auth.js");
 const {
   ConnectionRequestModel,
 } = require("../models/connectionRequest-models.js");
+const UserModel = require("../models/user-models.js");
 const userRouter = express();
 
 //get the connections of user (accepted)
@@ -26,16 +27,14 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
     //check that send data of sender only
     const data = isMatchConnection.map((data) => {
-        if(data.fromUserId._id.toString() === LoggedInUser._id.toString()){
-            return data.toUserId
-        }else{
-            return data.fromUserId
-        }
-    })
+      if (data.fromUserId._id.toString() === LoggedInUser._id.toString()) {
+        return data.toUserId;
+      } else {
+        return data.fromUserId;
+      }
+    });
 
-    return res
-      .status(200)
-      .json({ message: "Your Connections", data });
+    return res.status(200).json({ message: "Your Connections", data });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -61,6 +60,49 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     return res
       .status(200)
       .json({ message: "Your Requests", data: isRequestUser });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+//get the other user data for feed (Feed API)
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    // take LoggedInUser 
+    const LoggedInUser = req.user;
+
+    //check connection Request that not need 
+    const connectionRequest = await ConnectionRequestModel.find({
+        $or: [
+            {fromUserId: LoggedInUser._id},
+            {toUserId: LoggedInUser._id}
+        ]
+    }).select("fromUserId toUserId")
+
+    //remove duplicate using set data structure
+    const uniqueCnnectionRequest = new Set();
+    connectionRequest.forEach((connectionRequest) => {
+        uniqueCnnectionRequest.add(connectionRequest.fromUserId);
+        uniqueCnnectionRequest.add(connectionRequest.toUserId);
+    })
+    // console.log(uniqueCnnectionRequest)
+
+    //check and return all user data by excluding this uniqueConnectionRequest
+    const users = await UserModel.find({
+        $and: [
+            {_id: {$ne : LoggedInUser._id}},
+            {_id: {$nin : Array.from(uniqueCnnectionRequest)}}
+        ]
+    }).select("firstName lastName age gender about skills photoUrl")
+    // console.log(users)
+
+    //check users are there for feed
+    if(!users){
+        return res.status(400).json({message: "No User Found."})
+    }
+
+    return res.status(200).json({message: "Your Feeds", users})
+
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
