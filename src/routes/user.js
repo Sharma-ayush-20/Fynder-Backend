f4d19964b813782fe9acc2bce794c8a3c57626a0
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-12-02 17:04:07 +0530

    basic implementation of websocket, joinChat, sendMessage, disconnect and roomID

--- src/routes/user.js
+++ src/routes/user.js
@@ -16,7 +16,10 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       status: "accepted",
       $or: [{ toUserId: LoggedInUser._id }, { fromUserId: LoggedInUser._id }],
     })
-      .populate("fromUserId", "firstName lastName age gender about skills photoUrl")
+      .populate(
+        "fromUserId",
+        "firstName lastName age gender about skills photoUrl"
+      )
       .populate("toUserId", "firstName lastName age gender about skills");
 
     if (isMatchConnection.length === 0) {
@@ -26,13 +29,25 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     }
 
     //check that send data of sender only
-    const data = isMatchConnection.map((data) => {
-      if (data.fromUserId._id.toString() === LoggedInUser._id.toString()) {
-        return data.toUserId;
-      } else {
-        return data.fromUserId;
-      }
-    });
+    // const data = isMatchConnection.map((data) => {
+    //   if (data.fromUserId._id.toString() === LoggedInUser._id.toString()) {
+    //     return data.toUserId;
+    //   } else {
+    //     return data.fromUserId;
+    //   }
+    // });
+    const data = isMatchConnection
+      .map((data) => {
+        const from = data.fromUserId;
+        const to = data.toUserId;
+
+        if (from && from._id.toString() === LoggedInUser._id.toString()) {
+          return to;
+        } else {
+          return from;
+        }
+      })
+      .filter(Boolean); // remove null values
 
     return res.status(200).json({ message: "Your Connections", data });
   } catch (error) {
@@ -68,46 +83,47 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 //get the other user data for feed (Feed API)
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
-    // take LoggedInUser 
+    // take LoggedInUser
     const LoggedInUser = req.user;
 
     //add pagination for api
-    const limit = (req.query.limit) || 10;
-    const page = (req.query.page) || 1;
-    const skip = (page-1)*limit;
- 
-    //check connection Request that not need 
+    const limit = req.query.limit || 10;
+    const page = req.query.page || 1;
+    const skip = (page - 1) * limit;
+
+    //check connection Request that not need
     const connectionRequest = await ConnectionRequestModel.find({
-        $or: [
-            {fromUserId: LoggedInUser._id},
-            {toUserId: LoggedInUser._id}
-        ]
-    }).select("fromUserId toUserId")
+      $or: [{ fromUserId: LoggedInUser._id }, { toUserId: LoggedInUser._id }],
+    }).select("fromUserId toUserId");
 
     //remove duplicate using set data structure
     const uniqueCnnectionRequest = new Set();
     connectionRequest.forEach((connectionRequest) => {
-        uniqueCnnectionRequest.add(connectionRequest.fromUserId);
-        uniqueCnnectionRequest.add(connectionRequest.toUserId);
-    })
+      uniqueCnnectionRequest.add(connectionRequest.fromUserId);
+      uniqueCnnectionRequest.add(connectionRequest.toUserId);
+    });
     // console.log(uniqueCnnectionRequest)
 
     //check and return all user data by excluding this uniqueConnectionRequest
     const users = await UserModel.find({
-        $and: [
-            {_id: {$ne : LoggedInUser._id}},
-            {_id: {$nin : Array.from(uniqueCnnectionRequest)}}
-        ]
-    }).select("firstName lastName age gender about skills photoUrl isPremium memberShipType premiumExpiry").skip(skip).limit(limit)
+      $and: [
+        { _id: { $ne: LoggedInUser._id } },
+        { _id: { $nin: Array.from(uniqueCnnectionRequest) } },
+      ],
+    })
+      .select(
+        "firstName lastName age gender about skills photoUrl isPremium memberShipType premiumExpiry"
+      )
+      .skip(skip)
+      .limit(limit);
     // console.log(users)
 
     //check users are there for feed
-    if(!users){
-        return res.status(400).json({message: "No User Found."})
+    if (!users) {
+      return res.status(400).json({ message: "No User Found." });
     }
 
-    return res.status(200).json({message: "Your Feeds", users})
-
+    return res.status(200).json({ message: "Your Feeds", users });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,13 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+//create a hash room id
+const generateRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -11,18 +20,20 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     //handle events
 
-    socket.on("joinChat", () => {
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      //create room
+      const roomId = generateRoomId(userId, targetUserId);
+      console.log(`${firstName} join this ${roomId}`);
+      socket.join(roomId);
+    });
 
-    })
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = generateRoomId(userId, targetUserId);
+      console.log(`${firstName}: ${text}`);
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
 
-    socket.on("sendMessage", () => {
-
-    })
-
-    socket.on("disconnect", () => {
-
-    })
-    
+    socket.on("disconnect", () => {});
   });
 };
 
