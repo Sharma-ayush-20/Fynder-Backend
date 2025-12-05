28ffd75911633652c73efb1458750d8c10ccee6f
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-12-05 15:33:41 +0530

    cloudinary error check

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -2,14 +2,14 @@ const cloudinary = require("cloudinary").v2;
 
 const connectCloudinary = () => {
   cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_SECRET_kEY,
   });
 
   console.log("Cloudinary Connected:", process.env.CLOUDINARY_API_KEY ? "YES" : "NO");
-  console.log("Cloudinary Connected:", process.env.CLOUDINARY_CLOUD_NAME ? "YES" : "NO");
-  console.log("Cloudinary Connected:", process.env.CLOUDINARY_API_SECRET ? "YES" : "NO");
+  console.log("Cloudinary Connected:", process.env.CLOUDINARY_NAME ? "YES" : "NO");
+  console.log("Cloudinary Connected:", process.env.CLOUDINARY_SECRET_kEY ? "YES" : "NO");
 
 };
 
